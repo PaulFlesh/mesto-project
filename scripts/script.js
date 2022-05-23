2348ea8629e8197aa69eb3068944a9578903cfe5
Johnny Blade <46377053+PaulFlesh@users.noreply.github.com>
2022-05-23 08:15:26 +0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -7,5 +7,7 @@ function popupToggle() {
   }
 }
 
-document.querySelector('.popup__edit-button').addEventListener('click', popupToggle);
-document.querySelector('.popup__close-button').addEventListener('click', popupToggle);
+let popbtn = document.querySelector('.popup__edit-button');
+popbtn.addEventListener('click', popupToggle);
+let popbttn = document.querySelector('.popup__close-button');
+popbttn.addEventListener('click', popupToggle);
