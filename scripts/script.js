05e11bc7c75edfa47380c719b17710237822ddbd
Johnny Blade <nikitapiskurev@gmail.com>
2022-06-05 19:26:07 +0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -139,7 +139,7 @@ addPlaceButton.addEventListener('click', function () {
   addPlace(placeName.value, placeImage.value);
   placeName.value = '';
   placeImage.value = '';
-  handleClickImage();
+  handleClickPlace();
 });
 
 const renderCard = function(data, container) {
