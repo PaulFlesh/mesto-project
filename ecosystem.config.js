f7c74dd85f41027682005795599349ccb5e29386
Johnny Blade <notjohnnyblade@yandex.ru>
2024-02-03 19:43:18 +0400

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'post-deploy': 'cd ~/mesto-project/current && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'cd ~/mesto-project/current && pwd && npm ci && npm run build',
     },
   },
 }
